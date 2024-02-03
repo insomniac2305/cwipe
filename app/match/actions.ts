6e7ec818eff3df0418ba1f00ee2c5a031bf68911
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-02-03 17:43:08 +0100

    Style movie information, add icon

--- app/match/actions.ts
+++ app/match/actions.ts
@@ -8,6 +8,23 @@ const movieData: DiscoverMovies = {
   results: [
     {
       adult: false,
+      backdrop_path: "/tLxjbT5ROZRwYcpNT3nfQbqkApk.jpg",
+      genre_ids: [878, 12, 28],
+      id: 609681,
+      original_language: "en",
+      original_title: "The Marvels",
+      overview:
+        "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan Kamala Khan, aka Ms. Marvel, and Carol’s estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team up and learn to work in concert to save the universe.",
+      popularity: 1292.419,
+      poster_path: "/9GBhzXMFjgcZ3FdR9w3bUMMTps5.jpg",
+      release_date: "2023-11-08",
+      title: "The Marvels",
+      video: false,
+      vote_average: 6.4,
+      vote_count: 1331,
+    },
+    {
+      adult: false,
       backdrop_path: "/cnqwv5Uz3UW5f086IWbQKr3ksJr.jpg",
       genre_ids: [28, 12, 14],
       id: 572802,
@@ -73,23 +90,6 @@ const movieData: DiscoverMovies = {
       video: false,
       vote_average: 7.9,
       vote_count: 397,
-    },
-    {
-      adult: false,
-      backdrop_path: "/tLxjbT5ROZRwYcpNT3nfQbqkApk.jpg",
-      genre_ids: [878, 12, 28],
-      id: 609681,
-      original_language: "en",
-      original_title: "The Marvels",
-      overview:
-        "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan Kamala Khan, aka Ms. Marvel, and Carol’s estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team up and learn to work in concert to save the universe.",
-      popularity: 1292.419,
-      poster_path: "/9GBhzXMFjgcZ3FdR9w3bUMMTps5.jpg",
-      release_date: "2023-11-08",
-      title: "The Marvels",
-      video: false,
-      vote_average: 6.4,
-      vote_count: 1331,
     },
     {
       adult: false,
--- app/match/movieStack.tsx
+++ app/match/movieStack.tsx
@@ -5,6 +5,7 @@ import { rateMovie } from "@/app/match/actions";
 import SwipeCard from "@/app/match/swipeCard";
 import Image from "next/image";
 import { useState } from "react";
+import { HiMiniStar } from "react-icons/hi2";
 
 export default function MovieStack({ movies }: { movies: DiscoverMovies["results"] }) {
   const [swipedMovies, setSwipedMovies] = useState<Array<number>>([]);
@@ -24,14 +25,29 @@ export default function MovieStack({ movies }: { movies: DiscoverMovies["results
                 onSwipeRight={handleSwipe.bind(null, movie.id, true)}
               >
                 <div className="relative bg-gray-900 h-full w-full">
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL}/original${movie.poster_path}`}
-                    fill={true}
-                    className="object-cover opacity-0 transition-opacity duration-500"
-                    onLoad={(e) => ((e.target as HTMLImageElement).style.opacity = "1")}
-                    alt={movie.title}
-                    draggable={false}
-                  />
+                  <div className="relative h-[90%] w-full">
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL}/original${movie.poster_path}`}
+                      fill={true}
+                      className="object-cover opacity-0 transition-opacity duration-500"
+                      onLoad={(e) => ((e.target as HTMLImageElement).style.opacity = "1")}
+                      alt={movie.title}
+                      draggable={false}
+                    />
+                  </div>
+                  <div className="bg-gradient-to-t from-gray-900 from-40% via-gray-900/90 via-70% h-1/3 w-full absolute bottom-0 p-6 flex flex-col gap-2 justify-center">
+                    <h1 className="w-full text-2xl font-bold text-gray-50 whitespace-nowrap text-ellipsis overflow-hidden">
+                      {movie.title}
+                    </h1>
+                    <ul className="flex gap-2 text-gray-200 text-xs">
+                      <li className="p-2 bg-slate-700 rounded-full">Science Fiction</li>
+                      <li className="p-2 bg-slate-700 rounded-full">Action</li>
+                    </ul>
+                    <p className="text-sm text-gray-200">
+                      <HiMiniStar className="inline relative top-[1px] align-baseline" /> {movie.vote_average} -{" "}
+                      {movie.release_date.slice(0, 4)} - 1h 45m
+                    </p>
+                  </div>
                 </div>
               </SwipeCard>
             </li>

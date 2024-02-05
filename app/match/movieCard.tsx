a85e6000c2ada2948f0ca7fa687a83aaaf03fef9
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-02-05 23:20:49 +0100

    Separate movieCard into component

---
+++ app/match/movieCard.tsx
@@ -0,0 +1,67 @@
+import { DiscoverMovies } from "@/app/lib/definitions";
+import SwipeCard from "@/app/match/swipeCard";
+import Image from "next/image";
+import { HiArrowUturnLeft, HiHandThumbDown, HiHandThumbUp, HiInformationCircle, HiMiniStar } from "react-icons/hi2";
+
+export default function MovieCard({
+  movie,
+  handleSwipe,
+}: {
+  movie: DiscoverMovies["results"][number];
+  handleSwipe: (id: number, isLiked: boolean) => void;
+}) {
+  return (
+    <>
+      <SwipeCard
+        onSwipeLeft={handleSwipe.bind(null, movie.id, false)}
+        onSwipeRight={handleSwipe.bind(null, movie.id, true)}
+      >
+        <div className="relative bg-gray-900 h-full w-full">
+          <div className="relative h-[90%] w-full">
+            <Image
+              src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL}/original${movie.poster_path}`}
+              fill={true}
+              className="object-cover opacity-0 transition-opacity duration-500"
+              onLoad={(e) => ((e.target as HTMLImageElement).style.opacity = "1")}
+              alt={movie.title}
+              draggable={false}
+            />
+          </div>
+          <div className="bg-gradient-to-t from-gray-900 from-40% via-gray-900/90 via-70% h-72 w-full absolute bottom-0 p-6 flex flex-col gap-2 justify-center">
+            <h1 className="w-full text-2xl font-bold text-gray-50 whitespace-nowrap text-ellipsis overflow-hidden">
+              {movie.title}
+            </h1>
+            <ul className="flex gap-2 text-gray-200 text-xs">
+              <li className="p-2 bg-slate-700 rounded-full">Science Fiction</li>
+              <li className="p-2 bg-slate-700 rounded-full">Action</li>
+            </ul>
+            <p className="text-sm text-gray-200">
+              <HiMiniStar className="inline relative top-[1px] align-baseline" /> {movie.vote_average} -{" "}
+              {movie.release_date.slice(0, 4)} - 1h 45m
+            </p>
+          </div>
+        </div>
+      </SwipeCard>
+      <div className="flex absolute bottom-0 p-4 justify-evenly w-full items-center">
+        <button className="bg-slate-500 p-2 rounded-full text-gray-50 text-2xl">
+          <HiArrowUturnLeft />
+        </button>
+        <button
+          className="bg-pink-600 p-2 rounded-full text-gray-50 text-4xl"
+          onClick={handleSwipe.bind(null, movie.id, false)}
+        >
+          <HiHandThumbDown />
+        </button>
+        <button
+          className="bg-indigo-600 p-2 rounded-full text-gray-50 text-4xl"
+          onClick={handleSwipe.bind(null, movie.id, true)}
+        >
+          <HiHandThumbUp />
+        </button>
+        <button className="bg-slate-500 p-2 rounded-full text-gray-50 text-2xl">
+          <HiInformationCircle />
+        </button>
+      </div>
+    </>
+  );
+}
--- app/match/movieStack.tsx
+++ app/match/movieStack.tsx
@@ -2,10 +2,8 @@
 
 import { DiscoverMovies } from "@/app/lib/definitions";
 import { rateMovie } from "@/app/match/actions";
-import SwipeCard from "@/app/match/swipeCard";
-import Image from "next/image";
+import MovieCard from "@/app/match/movieCard";
 import { useState } from "react";
-import { HiArrowUturnLeft, HiHandThumbDown, HiHandThumbUp, HiInformationCircle, HiMiniStar } from "react-icons/hi2";
 
 export default function MovieStack({ movies }: { movies: DiscoverMovies["results"] }) {
   const [swipedMovies, setSwipedMovies] = useState<Array<number>>([]);
@@ -20,50 +18,7 @@ export default function MovieStack({ movies }: { movies: DiscoverMovies["results
         (movie, index) =>
           swipedMovies.includes(movie.id) || (
             <li className={`absolute top-0 h-full w-full`} key={movie.id} style={{ zIndex: movies.length - index }}>
-              <SwipeCard
-                onSwipeLeft={handleSwipe.bind(null, movie.id, false)}
-                onSwipeRight={handleSwipe.bind(null, movie.id, true)}
-              >
-                <div className="relative bg-gray-900 h-full w-full">
-                  <div className="relative h-[90%] w-full">
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL}/original${movie.poster_path}`}
-                      fill={true}
-                      className="object-cover opacity-0 transition-opacity duration-500"
-                      onLoad={(e) => ((e.target as HTMLImageElement).style.opacity = "1")}
-                      alt={movie.title}
-                      draggable={false}
-                    />
-                  </div>
-                  <div className="bg-gradient-to-t from-gray-900 from-40% via-gray-900/90 via-70% h-72 w-full absolute bottom-0 p-6 flex flex-col gap-2 justify-center">
-                    <h1 className="w-full text-2xl font-bold text-gray-50 whitespace-nowrap text-ellipsis overflow-hidden">
-                      {movie.title}
-                    </h1>
-                    <ul className="flex gap-2 text-gray-200 text-xs">
-                      <li className="p-2 bg-slate-700 rounded-full">Science Fiction</li>
-                      <li className="p-2 bg-slate-700 rounded-full">Action</li>
-                    </ul>
-                    <p className="text-sm text-gray-200">
-                      <HiMiniStar className="inline relative top-[1px] align-baseline" /> {movie.vote_average} -{" "}
-                      {movie.release_date.slice(0, 4)} - 1h 45m
-                    </p>
-                  </div>
-                </div>
-              </SwipeCard>
-              <div className="flex absolute bottom-0 p-4 justify-evenly w-full items-center">
-                <button className="bg-slate-500 p-2 rounded-full text-gray-50 text-2xl">
-                  <HiArrowUturnLeft />
-                </button>
-                <button className="bg-pink-600 p-2 rounded-full text-gray-50 text-4xl">
-                  <HiHandThumbDown />
-                </button>
-                <button className="bg-indigo-600 p-2 rounded-full text-gray-50 text-4xl">
-                  <HiHandThumbUp />
-                </button>
-                <button className="bg-slate-500 p-2 rounded-full text-gray-50 text-2xl">
-                  <HiInformationCircle />
-                </button>
-              </div>
+              <MovieCard movie={movie} handleSwipe={handleSwipe} />
             </li>
           )
       )}

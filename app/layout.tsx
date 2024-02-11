16c93235a018ef5a8391e303ddbe78c14fc02ce1
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-02-11 18:17:00 +0100

    Add NextUI packages and config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import { Providers } from "@/app/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={clsx(inter.className, "overflow-hidden")}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

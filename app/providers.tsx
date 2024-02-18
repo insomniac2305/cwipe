4aa50306047d2585b051cad3f31d1f20620690b5
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-02-18 23:32:39 +0100

    Add config provider with default localization options

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import ConfigProvider from "@/app/configProvider";
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <ConfigProvider>
+      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+    </ConfigProvider>
+  );
 }

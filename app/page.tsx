81d2e56443306c34a5059aca50837428c67a5e01
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-02-22 10:24:15 +0100

    Change height to dynamic viewport units

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-dvh flex-col items-center">
       Home
       <Link href="/match">Match</Link>
     </main>

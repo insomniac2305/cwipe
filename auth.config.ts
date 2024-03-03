e2ea2dedbf6b562fdd784daa8e3dbd547f3b9829
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-03-03 17:38:00 +0100

    Merge Google and Anonymous Account on sign in

--- auth.config.ts
+++ auth.config.ts
@@ -1,13 +1,32 @@
+import vercelPostgresAdapter from "@/app/lib/vercelPostgresAdapter";
 import { randomUUID } from "crypto";
 import type { NextAuthConfig } from "next-auth";
+import {} from "next-auth/jwt";
+import { AdapterUser } from "next-auth/adapters";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { z } from "zod";
+
+declare module "next-auth" {
+  interface User {
+    isProviderAccount?: boolean;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    isProviderAccount?: boolean;
+  }
+}
+
+const dbAdapter = vercelPostgresAdapter();
 
 export const authConfig = {
   pages: {
     signIn: "/login",
   },
+  adapter: dbAdapter,
+  session: { strategy: "jwt" },
   providers: [
     GoogleProvider,
     CredentialsProvider({
@@ -17,10 +36,17 @@ export const authConfig = {
           .safeParse(credentials);
 
         if (parsedCredentials.success) {
-          const user = {
+          let user: AdapterUser = {
             id: randomUUID(),
             name: parsedCredentials.data.name,
+            email: randomUUID(),
+            emailVerified: null,
           };
+
+          if (dbAdapter.createUser) {
+            user = await dbAdapter.createUser(user);
+          }
+
           return user;
         } else {
           return null;
@@ -28,4 +54,38 @@ export const authConfig = {
       },
     }),
   ],
+  callbacks: {
+    jwt: ({ token, user, profile, trigger }) => {
+      if (user?.id && profile && trigger === "signIn" && dbAdapter.updateUser) {
+        const updateUser: Partial<AdapterUser> & Pick<AdapterUser, "id"> = {
+          id: user.id,
+          name: profile.name || (user.name as string),
+          email: profile.email || (user.email as string),
+          image: profile.picture || user.image,
+        };
+        dbAdapter.updateUser(updateUser);
+
+        return {
+          ...token,
+          name: updateUser.name,
+          email: updateUser.email,
+          picture: updateUser.image,
+          isProviderAccount: true,
+        };
+      }
+      return token;
+    },
+    session: ({ session, token }) => ({
+      ...session,
+      sub: token.sub,
+      user: {
+        ...session.user,
+        id: token.sub,
+        name: token.name,
+        email: token.email,
+        image: token.picture,
+        isProviderAccount: token.isProviderAccount,
+      },
+    }),
+  },
 } satisfies NextAuthConfig;

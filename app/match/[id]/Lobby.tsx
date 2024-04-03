4600258322902eb2d53058b7c6c1e9c21501e9aa
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-04-03 23:21:14 +0200

    Add navigator.share fallback and refactor

--- app/match/[id]/Lobby.tsx
+++ app/match/[id]/Lobby.tsx
@@ -3,24 +3,71 @@
 import { User } from "@/app/components/User";
 import { MatchSession } from "@/app/lib/definitions";
 import { Button } from "@nextui-org/react";
-import { usePathname } from "next/navigation";
-import { FaPlus } from "react-icons/fa6";
+import { useEffect, useState } from "react";
+import { FaCheck, FaPlus } from "react-icons/fa6";
+
+function ShareButton({ id }: { id: string }) {
+  const [isLinkCopied, setIsLinkCopied] = useState(false);
+
+  const shareLink = () => {
+    const baseUrl = process.env.NEXT_PUBLIC_URL;
+    const shareUrl = `${baseUrl}/match/${id}`;
+    const shareData = {
+      title: "Movie Matching Session",
+      text: "Join me on Cwipe to find out what movie we can watch together!",
+      url: shareUrl,
+    };
+    if (navigator.share) {
+      navigator.share(shareData);
+    } else {
+      try {
+        navigator.clipboard.writeText(shareUrl);
+        setIsLinkCopied(true);
+      } catch (error) {
+        alert("Error writing to clipboard");
+      }
+    }
+  };
+
+  useEffect(() => {
+    let id: NodeJS.Timeout;
+    if (isLinkCopied) {
+      id = setTimeout(() => {
+        setIsLinkCopied(false);
+      }, 5000);
+    }
+    return () => clearTimeout(id);
+  }, [isLinkCopied]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Button
+        isIconOnly
+        startContent={
+          isLinkCopied ? (
+            <FaCheck className="text-3xl" />
+          ) : (
+            <FaPlus className="text-3xl" />
+          )
+        }
+        className="h-20 w-20"
+        variant="flat"
+        radius="full"
+        color={isLinkCopied ? "success" : "default"}
+        onPress={shareLink}
+      />
+      <span className="w-20 text-center text-sm">
+        {isLinkCopied ? "Link copied" : "Invite"}
+      </span>
+    </div>
+  );
+}
 
 export default function Lobby({
   matchSession,
 }: {
   matchSession: MatchSession;
 }) {
-  const pathname = usePathname();
-
-  const shareLink = () => {
-    const baseUrl = process.env.NEXT_PUBLIC_URL;
-    const shareUrl = `${baseUrl}${pathname}`;
-    if (navigator.share) {
-      navigator.share({ url: shareUrl });
-    }
-  };
-
   return (
     <main className="flex h-dvh items-center justify-center overflow-hidden">
       <div className="flex h-full w-full flex-col overflow-auto p-8 md:max-h-[40rem] md:max-w-screen-md">
@@ -33,17 +80,7 @@ export default function Lobby({
               imageSrc={user.image || undefined}
             />
           ))}
-          <div className="flex flex-col gap-2">
-            <Button
-              isIconOnly
-              startContent={<FaPlus className="text-3xl" />}
-              className="h-20 w-20"
-              variant="flat"
-              radius="full"
-              onPress={shareLink}
-            />
-            <span className="w-20 text-center text-sm">Invite</span>
-          </div>
+          <ShareButton id={matchSession.id} />
         </div>
         <div className="flex justify-center">
           <Button color="primary">Start matching</Button>

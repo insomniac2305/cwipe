822b97009e2c4742bb1bfbc8b164d053a0119759
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-04-04 21:50:41 +0200

    Refactor Lobby

--- app/match/[id]/Lobby.tsx
+++ app/match/[id]/Lobby.tsx
@@ -3,65 +3,7 @@
 import { User } from "@/app/components/User";
 import { MatchSession } from "@/app/lib/definitions";
 import { Button } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import { FaCheck, FaPlus } from "react-icons/fa6";
-
-function ShareButton({ id }: { id: string }) {
-  const [isLinkCopied, setIsLinkCopied] = useState(false);
-
-  const shareLink = () => {
-    const baseUrl = process.env.NEXT_PUBLIC_URL;
-    const shareUrl = `${baseUrl}/match/${id}`;
-    const shareData = {
-      title: "Movie Matching Session",
-      text: "Join me on Cwipe to find out what movie we can watch together!",
-      url: shareUrl,
-    };
-    if (navigator.share) {
-      navigator.share(shareData);
-    } else {
-      try {
-        navigator.clipboard.writeText(shareUrl);
-        setIsLinkCopied(true);
-      } catch (error) {
-        alert("Error writing to clipboard");
-      }
-    }
-  };
-
-  useEffect(() => {
-    let id: NodeJS.Timeout;
-    if (isLinkCopied) {
-      id = setTimeout(() => {
-        setIsLinkCopied(false);
-      }, 5000);
-    }
-    return () => clearTimeout(id);
-  }, [isLinkCopied]);
-
-  return (
-    <div className="flex flex-col gap-2">
-      <Button
-        isIconOnly
-        startContent={
-          isLinkCopied ? (
-            <FaCheck className="text-3xl" />
-          ) : (
-            <FaPlus className="text-3xl" />
-          )
-        }
-        className="h-20 w-20"
-        variant="flat"
-        radius="full"
-        color={isLinkCopied ? "success" : "default"}
-        onPress={shareLink}
-      />
-      <span className="w-20 text-center text-sm">
-        {isLinkCopied ? "Link copied" : "Invite"}
-      </span>
-    </div>
-  );
-}
+import { ShareButton } from "@/app/match/[id]/ShareButton";
 
 export default function Lobby({
   matchSession,
@@ -76,7 +18,7 @@ export default function Lobby({
           {matchSession.users.map((user, index) => (
             <User
               key={user.id}
-              name={user.name || `User #${index}`}
+              name={user.name || `User ${index}`}
               imageSrc={user.image || undefined}
             />
           ))}

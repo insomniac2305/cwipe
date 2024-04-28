124c606ea348a0ae0c9d23708d8e0e66afb71d42
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-04-28 12:58:52 +0200

    Show message when no match sessions found

--- app/match/page.tsx
+++ app/match/page.tsx
@@ -10,20 +10,24 @@ export default async function Match() {
     <main className="flex h-dvh items-center justify-center overflow-hidden">
       <div className="flex h-full w-full flex-col gap-2 p-8 md:max-h-[40rem] md:max-w-screen-md">
         <h1 className="mb-2 font-heading text-2xl">Match Sessions</h1>
-        {error ? (
-          <div className="flex flex-1 items-center justify-center">
+        <div className="flex flex-1 flex-col items-center justify-center">
+          {error ? (
             <ErrorMessage>{error.message}</ErrorMessage>
-          </div>
-        ) : (
-          <ul className="flex flex-1 flex-col gap-2">
-            {matchSessions.map((matchSession) => (
-              <MatchSessionItem
-                key={matchSession.id}
-                matchSession={matchSession}
-              />
-            ))}
-          </ul>
-        )}
+          ) : matchSessions.length === 0 ? (
+            <p className="rounded-2xl bg-content1 p-4 text-center font-medium">
+              There are no active sessions, click below to get started
+            </p>
+          ) : (
+            <ul className="flex flex-1 flex-col gap-2 self-stretch">
+              {matchSessions.map((matchSession) => (
+                <MatchSessionItem
+                  key={matchSession.id}
+                  matchSession={matchSession}
+                />
+              ))}
+            </ul>
+          )}
+        </div>
         <form className="self-center" action={createMatchSession}>
           <Button color="primary" type="submit">
             Start session

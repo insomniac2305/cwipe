87279daf798d3e5d751629a3f6236041f97dd992
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-11-03 19:04:23 +0100

    Verify completed onboarding before match

--- app/match/layout.tsx
+++ app/match/layout.tsx
@@ -1,11 +1,19 @@
+import { auth } from "@/app/lib/auth";
 import MatchLayout from "@/app/match/MatchLayout";
 import { MatchSessionSidebar } from "@/app/match/MatchSessionSidebar";
+import { verifyOnboardingComplete } from "@/app/onboarding/actions";
+import { redirect } from "next/navigation";
 
 export default async function Match({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+  const userId = session?.user?.id;
+  const isOnboardingComplete = await verifyOnboardingComplete(userId);
+  if (!isOnboardingComplete) redirect("/onboarding");
+
   return (
     <MatchLayout sidebar={<MatchSessionSidebar />}>{children}</MatchLayout>
   );

9d8c825940b17e7256a6facd629fbe81ac3413e2
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-11-10 16:08:02 +0100

    Add watch providers to match list

--- app/match/[id]/MatchListItem.tsx
+++ app/match/[id]/MatchListItem.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "@nextui-org/react";
+import { Avatar, AvatarGroup, Link } from "@nextui-org/react";
 import { Movie } from "@/app/lib/definitions";
 import NextImage from "next/image";
 import { MovieKeyFacts } from "@/app/match/[id]/MovieKeyFacts";
 
 export async function MatchListItem({ movie }: { movie: Movie }) {
   return (
-    <li className="flex w-full min-w-80 items-center gap-2 rounded-2xl bg-content2 shadow-lg">
+    <li className="flex w-full min-w-80 gap-2 rounded-2xl bg-content2 shadow-lg">
       <Link href={movie.watch_providers?.link} isExternal className="shrink-0">
         <NextImage
           height={192}
@@ -22,7 +22,7 @@ export async function MatchListItem({ movie }: { movie: Movie }) {
         />
       </Link>
       <div className="flex h-full flex-col gap-2 px-2 py-4">
-        <h2 className="line-clamp-2 overflow-ellipsis font-heading">
+        <h2 className="line-clamp-2 overflow-ellipsis font-heading text-lg">
           {movie.title}
         </h2>
         <MovieKeyFacts
@@ -30,6 +30,18 @@ export async function MatchListItem({ movie }: { movie: Movie }) {
           releaseDate={movie.release_date}
           runtime={movie.runtime}
         />
+        <div className="flex justify-start">
+          <AvatarGroup max={5} isGrid className="gap-2">
+            {movie.watch_providers?.flatrate?.map((watchProvider) => (
+              <Avatar
+                key={watchProvider.provider_id + movie.id}
+                src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL}/original${watchProvider.logo_path}`}
+                name={watchProvider.provider_name}
+                radius="md"
+              />
+            ))}
+          </AvatarGroup>
+        </div>
       </div>
     </li>
   );

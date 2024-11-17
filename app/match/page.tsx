ad9ef6d2878253e3f1a826672c2ac221a39d5eb0
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-11-17 21:23:02 +0100

    Adjust empty match page

--- app/match/page.tsx
+++ app/match/page.tsx
@@ -1,7 +1,13 @@
+import { PiCardsThreeFill } from "react-icons/pi";
+
 export default async function Match() {
   return (
-    <div className="flex items-center justify-center">
-      Select a match session
+    <div className="flex h-full w-full flex-col items-center justify-center gap-2">
+      <PiCardsThreeFill className="text-5xl" />
+      <p className="text-center text-2xl">
+        Select a match session <br />
+        or start a new one
+      </p>
     </div>
   );
 }

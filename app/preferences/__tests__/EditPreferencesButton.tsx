6e6229ec8818f1a0300c95fcdc005618e458feb0
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2024-12-05 14:14:50 +0100

    Move mock user preference object to mocked action

--- app/preferences/__tests__/EditPreferencesButton.tsx
+++ app/preferences/__tests__/EditPreferencesButton.tsx
@@ -1,58 +1,10 @@
+import { mockUserPreferences } from "@/app/preferences/lib/__mocks__/actions";
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { getUserPreferences } from "@/app/preferences/lib/actions";
 import { EditPreferencesButton } from "@/app/preferences/components/EditPreferencesButton";
 
 vi.mock("@/app/preferences/lib/actions");
-
-const mockUserPreferences = {
-  data: {
-    language: {
-      iso_639_1: "DE",
-      english_name: "German",
-      name: "Deutsch",
-    },
-    region: {
-      iso_3166_1: "DE",
-      english_name: "Germany",
-      native_name: "Deutschland",
-    },
-    providers: [
-      {
-        display_priority: 0,
-        logo_path: "",
-        provider_name: "",
-        provider_id: 1,
-      },
-      {
-        display_priority: 0,
-        logo_path: "",
-        provider_name: "",
-        provider_id: 2,
-      },
-      {
-        display_priority: 0,
-        logo_path: "",
-        provider_name: "",
-        provider_id: 3,
-      },
-    ],
-    genres: [
-      {
-        id: 4,
-        name: "",
-      },
-      {
-        id: 5,
-        name: "",
-      },
-      {
-        id: 6,
-        name: "",
-      },
-    ],
-  },
-};
 
 describe("Preferences", () => {
   it("renders link to onboarding page", async () => {
--- app/preferences/lib/__mocks__/actions.ts
+++ app/preferences/lib/__mocks__/actions.ts
@@ -1,3 +1,52 @@
 import { vi } from "vitest";
 
 export const getUserPreferences = vi.fn();
+
+export const mockUserPreferences = {
+  data: {
+    language: {
+      iso_639_1: "DE",
+      english_name: "German",
+      name: "Deutsch",
+    },
+    region: {
+      iso_3166_1: "DE",
+      english_name: "Germany",
+      native_name: "Deutschland",
+    },
+    providers: [
+      {
+        display_priority: 0,
+        logo_path: "",
+        provider_name: "",
+        provider_id: 1,
+      },
+      {
+        display_priority: 0,
+        logo_path: "",
+        provider_name: "",
+        provider_id: 2,
+      },
+      {
+        display_priority: 0,
+        logo_path: "",
+        provider_name: "",
+        provider_id: 3,
+      },
+    ],
+    genres: [
+      {
+        id: 4,
+        name: "",
+      },
+      {
+        id: 5,
+        name: "",
+      },
+      {
+        id: 6,
+        name: "",
+      },
+    ],
+  },
+};
